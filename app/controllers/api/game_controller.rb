3ccddcf1d5c9924f20a12dc155d22a4cca909111
George <gchriscarter@gmail.com>
2021-02-24 20:52:57 -0500

    add game controller, name and number json.jb, and routes

--- app/controllers/api/game_controller.rb
+++ app/controllers/api/game_controller.rb
@@ -1,2 +1,22 @@
 class Api::GameController < ApplicationController
+  def name
+    @name = params[:name].upcase
+    if @name[0] == "A"
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    end
+
+    render "name.json.jb"
+  end
+
+  def guess_number
+    @number = params[:number].to_i
+    if @number < 78
+      @message = "Too Low"
+    elsif @number > 78
+      @message = "Too High"
+    elsif @number == 78
+      @message = "You win!"
+    end
+    render "number.json.jb"
+  end
 end
